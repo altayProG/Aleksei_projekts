a22a413b737ccf323013e4e3a782e152851880da
Алексей <96436844+AlekseiBlindul@users.noreply.github.com>
2022-02-10 11:49:49 +0700

    Добавьте файлы проекта.

---
+++ PoneBook.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+#include <Windows.h>
+using namespace std;
+
+class FULLNAME
+{
+
+public:
+
+    string name = " ";
+    long long pone_number = 0;
+
+    FULLNAME() {}
+
+    FULLNAME(string name, long long pone_number) {
+
+        this->name = name;
+        this->pone_number = pone_number;
+    }
+
+    void Print() {
+
+        cout << "name: " << name << endl << "Pone number: " << pone_number << endl;
+    }
+};
+
+class WRITE :public FULLNAME
+{
+
+public:
+
+    WRITE() {
+        string file_txt = "full_info.txt";
+        ofstream fout;
+
+        fout.open(file_txt, ofstream::app);
+
+        if (!fout.is_open()) {
+            cout << "Ошибка открытия файла!\n";
+        }
+        else {
+            cout << "Фаил успешно открыт!\n";
+            fout.write((char*)&fullname, sizeof(FULLNAME));
+        }
+        fout.close();
+    }
+};
+
+int main()
+{
+    setlocale(0, "");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    WRITE write;
+    long long a = 0;
+    string f;
+    cin >> f >> a;
+    FULLNAME fullname(f, a);
+    fullname.Print();
+
+    return 0;
+
+}
